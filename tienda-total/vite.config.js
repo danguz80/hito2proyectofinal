71d9947cd0f016b82b91ce56fb286063d58e9947
Daniel Guzman Sagredo <60302875+danguz80@users.noreply.github.com>
2025-02-16 11:49:27 +0000

    Deployando 5

--- tienda-total/vite.config.js
+++ tienda-total/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/hito2proyectofinal/tienda-total/', // Asegura que las rutas sean correctas
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: 'dist', 
+    assetsDir: '', // IMPORTANTE: Deja vacío para que los assets no se aniden en "/assets"
   },
 });
