00f74868c0f240935adeabd7773e6de0caa686e0
Daniel Guzman Sagredo <60302875+danguz80@users.noreply.github.com>
2025-02-16 11:11:48 +0000

    Deployando 2

--- tienda-total/vite.config.js
+++ tienda-total/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hito2proyectofinal/tienda-total/",
+  base: "/tienda-total/",
   plugins: [react()]
 })
