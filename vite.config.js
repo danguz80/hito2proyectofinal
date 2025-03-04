35d878157a50d497421801dd4779144697331c30
Daniel Guzman Sagredo <danguz80@outlook.com>
2025-03-03 22:39:26 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hito3proyectofinal/', // 🔥 Volvemos a esto
-  build: {
-    outDir: 'dist',
-  },
+  base: "/hito3proyectofinal/", // 👈 Asegúrate de que el nombre coincide con tu repo en GitHub
 });
