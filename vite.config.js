9bda3bc85544f359dd2fa92cbae971fbdd200059
Daniel Guzman Sagredo <danguz80@outlook.com>
2025-03-03 22:35:22 -0300

    Configurar vite.config.js en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hito2proyectofinal/', // 🔥 Volvemos a esto
+  base: '/hito3proyectofinal/', // 🔥 Volvemos a esto
   build: {
     outDir: 'dist',
   },
